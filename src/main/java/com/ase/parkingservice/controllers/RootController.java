61608c73d5d287d55384562e3e43d26c84c89f0f
Jan Hoang <janhoang03@gmail.com>
2025-10-21 09:56:13 +0200

    Update API-Endpoint

--- src/main/java/com/ase/parkingservice/controllers/RootController.java
+++ src/main/java/com/ase/parkingservice/controllers/RootController.java
@@ -10,7 +10,7 @@ import com.ase.parkingservice.entities.Usage;
 import com.ase.parkingservice.services.UsageService;
 
 @RestController
-@RequestMapping("/api/v1/parkservice")
+@RequestMapping("/")
 public class RootController {
   private final UsageService usageService;
 
