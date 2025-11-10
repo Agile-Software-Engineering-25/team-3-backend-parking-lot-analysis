a8a7e56023008d6ed6a0a791418e04eabf90eac8
MoLetsPlui09 <moletsplui@gmail.com>
2025-11-10 09:33:34 +0100

    Create ExternalAPIService.java

---
+++ src/main/java/com/ase/parkingservice/services/ExternalAPIService.java
@@ -0,0 +1,28 @@
+package com.ase.parkingservice.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/**
+ * Service zum Abrufen und Verarbeiten externer API-Daten.
+ */
+@Service
+public class ExternalApiService {
+
+    private final WebClient webClient;
+
+    public ExternalApiService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+  
+    public List<ExternalParkingDto> fetchParkingData() {
+        return webClient.get()
+            .uri("https://sau-portal.de/team-11-api/api/v1/users?withDetails=false")
+            .retrieve()
+            .bodyToFlux(ExternalParkingDto.class)
+            .collectList()
+            .block(); // block() = synchroner Aufruf (für klassische Spring Services)
+    }
+}
