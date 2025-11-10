bd41d871f4f97434c3ca7fc932040541e15f349e
Jan Hoang <janhoang03@gmail.com>
2025-11-10 12:53:57 +0100

    update codestyle for checkstyle

--- src/main/java/com/ase/parkingservice/services/ExternalApiService.java
+++ src/main/java/com/ase/parkingservice/services/ExternalApiService.java
@@ -1,12 +1,10 @@
 package com.ase.parkingservice.services;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 import com.ase.parkingservice.entities.EmployeeDto;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Service zum Abrufen und Verarbeiten externer API-Daten.
@@ -14,17 +12,17 @@ import org.springframework.http.ResponseEntity;
 @Service
 public class ExternalApiService {
 
-    private final RestTemplate restTemplate;
+  private final RestTemplate restTemplate;
 
-    public ExternalApiService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
+  public ExternalApiService(RestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+  }
 
-    public List<EmployeeDto> fetchEmployeeData() {
-          String url = "https://sau-portal.de/team-11-api/api/v1/users?withDetails=false";
-          ResponseEntity<EmployeeDto[]> response =
-              restTemplate.getForEntity(url, EmployeeDto[].class);
-  
-          return List.of(response.getBody());
-      }
+  public List<EmployeeDto> fetchEmployeeData() {
+    String url = "https://sau-portal.de/team-11-api/api/v1/users?withDetails=false";
+    ResponseEntity<EmployeeDto[]> response =
+        restTemplate.getForEntity(url, EmployeeDto[].class);
+
+    return List.of(response.getBody());
+  }
 }
